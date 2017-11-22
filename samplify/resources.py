bc9562b92b61758daec04b8352429c64e0457cfe
Charlie Mak <charlie@samplify.io>
2017-11-22 13:06:47 +0800

    fix: proper conversion to dash case

--- samplify/resources.py
+++ samplify/resources.py
@@ -8,8 +8,8 @@ from samplify import api_requestor
 
 
 def camelcase_to_dashcase(name):
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1-\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1-\2', s1).lower()
 
 
 class SamplifyResource(object):
